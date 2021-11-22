e0ed9d3211b5e4d7b134be644fd898f4d961e319
Simon <simon.wulp@gmail.com>
2021-11-22 21:50:42 +0100

    Added models, updated PSM

--- src/turtlebot3_gazebo/scripts/ros_pose_sensor_mapper.py
+++ src/turtlebot3_gazebo/scripts/ros_pose_sensor_mapper.py
@@ -3,10 +3,13 @@ from __future__ import print_function
 
 import sys
 import math
+from typing import Set
 import rospy
 import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan, Image
+from gazebo_msgs.msg import ModelState 
+from gazebo_msgs.srv import SetModelState, GetModelState
 from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge, CvBridgeError
 from PIL import Image as ImagePIL
@@ -16,33 +19,39 @@ import message_filters
 class PoseSensorMapper:
 
     def __init__(self):
-        self.pose_sub = message_filters.Subscriber("/odom", Odometry)
+        rospy.wait_for_service('/gazebo/set_model_state')
+        try:
+           self.set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+        except rospy.ServiceException :
+            print("Service call failed")
+
+        self.state_msg = ModelState()
+        self.state_msg.model_name = 'turtlebot3_waffle_pi'
+
         self.laser_sub = message_filters.Subscriber("/scan", LaserScan)
         self.image_sub = message_filters.Subscriber("/camera/rgb/image_raw", Image)
 
-        self.ts = message_filters.ApproximateTimeSynchronizer([self.pose_sub, self.laser_sub, self.image_sub], 10, 1)
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.laser_sub, self.image_sub], 10, 1)
         self.ts.registerCallback(self.callback)
-        
+
         self.bridge = CvBridge()
 
-    def callback(self, pose_msg, laser_msg, image_msg):
+    def callback(self, laser_msg, image_msg):
         ## POSE
-
         # position coordinates
-        ros_pos = pose_msg.pose.pose.position
+        ros_pos = self.state_msg.pose.position
         ros_x = ros_pos.x
         ros_y = ros_pos.y
 
         # orientation quaternion to yaw in 0-360 degrees
-        ros_ori_quat = pose_msg.pose.pose.orientation
-        ros_ori_euler = trans.euler_from_quaternion([ros_ori_quat.x, ros_ori_quat.y, ros_ori_quat.z, ros_ori_quat.w])
-        ros_ori_yaw_deg = abs(math.degrees(ros_ori_euler[2]) % 360)
+        # ros_ori_quat = pose_msg.pose.pose.orientation
+        # ros_ori_euler = trans.euler_from_quaternion([ros_ori_quat.x, ros_ori_quat.y, ros_ori_quat.z, ros_ori_quat.w])
+        # ros_ori_yaw_deg = abs(math.degrees(ros_ori_euler[2]) % 360)
 
         # pose in (x-coordinate, y-coordinate, yaw in degrees)
-        ros_pose = (ros_x, ros_y, ros_ori_yaw_deg)
+        ros_pose = (ros_x, ros_y)
 
         ## LASER
-
         # laser ranges
         ranges = laser_msg.ranges
 
@@ -52,8 +61,20 @@ class PoseSensorMapper:
         except CvBridgeError as e:
             print(e)
 
-        cv2.imshow("Image window", cv_image)
+        # cv2.imshow("Image window", cv_image)
+
+        
+
+        self.state_msg.pose.position.x = -self.i
+        self.state_msg.pose.position.y = -self.i
+        
+        self.set_state(self.state_msg)
+
+        print('{}'.format(ros_pose))
         cv2.waitKey(3)
+
+        input('Press key')
+        self.i += 1
 
 
 def main():
