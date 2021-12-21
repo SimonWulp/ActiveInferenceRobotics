8876edd4acc02b241486592a4b27e9fbd3248b1c
SimonWulp <simon.wulp@gmail.com>
2021-12-21 15:31:30 +0100

    Update

--- src/turtlebot3_gazebo/scripts/ros_pose_sensor_mapper_warehouse.py
+++ src/turtlebot3_gazebo/scripts/ros_pose_sensor_mapper_warehouse.py
@@ -4,16 +4,12 @@ from __future__ import print_function
 import torch
 import math
 from typing import Set
-from genpy import message
 import rospy
 import cv2
-from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan, Image
 from gazebo_msgs.msg import ModelState 
 from gazebo_msgs.srv import SetModelState, GetModelState
-from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge, CvBridgeError
-from PIL import Image as ImagePIL
 from tf import transformations as trans
 import message_filters
 import pickle
@@ -25,11 +21,12 @@ class PoseSensorMapper:
         self.get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
         # Map params
-        self.map_x_range = (-4.2, 6.5)
-        self.map_y_range = (-9.9, 10.0)
+        self.map_x_range = (-4.2, 2.3)
+        self.map_y_range = (-9.8, 0.8)
 
-        self.step_size = 1.2
-        self.turn_step_size = 10
+        self.step_size = 0.125
+        self.turn_step_size = 16
+        self.range_samples = 360
 
         # DO NOT CHANGE
         self.x = self.map_x_range[0]
@@ -37,6 +34,7 @@ class PoseSensorMapper:
 
         self.turns = self.turn_step_size
         self.turning_mode = True
+        self.mappings_made = 0
         
         self.update_pose(self.x, self.y, 0)
 
@@ -49,19 +47,19 @@ class PoseSensorMapper:
         self.bridge = CvBridge()
 
         self.t_poses = torch.empty((0, 3), dtype=torch.float32)
-        self.t_ranges = torch.empty((0, 360), dtype=torch.float32)
-        self.t_images = torch.empty((0, 240, 320), dtype=torch.uint8)
+        self.t_ranges = torch.empty((0, self.range_samples), dtype=torch.float32)
+        self.t_images = torch.empty((0, 120, 240), dtype=torch.uint8)
+
+
 
     def callback(self, laser_msg, image_msg):
         callback_ended = True     
         # laser ranges
         ranges = laser_msg.ranges
-        # print(ranges)
 
         # camera image
         try:
-            cv_image = cv2.resize(self.bridge.imgmsg_to_cv2(image_msg, "mono8"), (320, 240), interpolation=cv2.INTER_AREA)
-            # cv_image = self.bridge.imgmsg_to_cv2(image_msg, "mono8")
+            cv_image = cv2.resize(self.bridge.imgmsg_to_cv2(image_msg, "mono8"), (240, 180), interpolation=cv2.INTER_AREA)[:120]
         except CvBridgeError as e:
             print(e)
         # cv2.imshow("Image window", cv_image)
@@ -94,16 +92,23 @@ class PoseSensorMapper:
                 rospy.signal_shutdown('Field mapped')
                 print("Entire environment is mapped.")
         
-        ros_pose = self.get_state('turtlebot3_waffle_pi', '').pose
+        # ros_pose = self.get_state('turtlebot3_waffle_pi', '').pose
         # print('Actual ROS pos: x: {:.6f}\ty: {:.6f}'.format(ros_pose.position.x, ros_pose.position.y, 6))
 
         if self.turning_mode and callback_ended:
             if self.turns > 0:
                 # im_str = '/home/simon/catkin_ws/src/turtlebot3_gazebo/scripts/data/' + str(self.x) + '_' + str(self.y) + '_' + str((self.turns*22.5)%360) + '.jpg'
-                self.t_poses = torch.cat((self.t_poses, torch.tensor([self.x, self.y, (360/self.turn_step_size)], dtype=torch.float32).unsqueeze(0)))                
+                self.t_poses = torch.cat((self.t_poses, torch.tensor([self.x, self.y, self.turns*(360/self.turn_step_size)], dtype=torch.float32).unsqueeze(0)))                
                 self.t_ranges = torch.cat((self.t_ranges, torch.tensor(ranges, dtype=torch.float32).unsqueeze(0)))
                 self.t_images = torch.cat((self.t_images, torch.tensor(cv_image, dtype=torch.uint8).unsqueeze(0)))
-                print('Mapping made at {}\t{}\t{}\n'.format(self.x, self.y, self.turns*(360/self.turn_step_size)))
+                print('Mapping made at {}\t{}\t{}'.format(self.x, self.y, self.turns*(360/self.turn_step_size)))
+                
+                self.mappings_made += 1
+                if self.mappings_made % 1000 == 0:
+                    full_pkl = (self.t_poses, self.t_ranges, self.t_images)
+                    with open('/home/simon/catkin_ws/src/turtlebot3_gazebo/scripts/data/data.pkl', 'wb') as f:
+                        pickle.dump(full_pkl, f)
+                    print('Checkpoint pickle dumped')
                 
                 self.update_pose(self.x, self.y, (self.turns-1)*(360/self.turn_step_size))
                 self.turns -= 1
@@ -112,8 +117,6 @@ class PoseSensorMapper:
                 self.turning_mode = False
                 self.turns = self.turn_step_size
 
-
-        
     def legal_pos(self, x, y):
         if self._in_field(x, y) and not self._in_obj(x, y):
             return True
@@ -125,6 +128,7 @@ class PoseSensorMapper:
             return True
         return False
 
+    # edit for objects in env
     def _in_obj(self, x, y):
         # forklift
         if x > -1.1 and x < 0.8 and y > -10 and y < -9:
@@ -132,19 +136,19 @@ class PoseSensorMapper:
         # boxes_low
         if x > -2.9 and x < -0.7 and y > -8.9 and y < -5.5:
             return True
-        # boxes_mid
-        if x > -2.8 and x < -0.1 and y > 1 and y < 6.6:
-            return True
-        # boxes high
-        if x > -2.5 and x < 1.4 and y > 7.1 and y < 10.2:
-            return True
-        # boxes top right
-        if x > 2.1 and x < 6.6 and y > 2.6 and y < 10.2:
-            return True
-        # scaffold
-        if x > 2.2 and x < 6.6 and y > -10 and y < 1.4:
-            return True
-        return False
+        # # boxes_mid
+        # if x > -2.8 and x < -0.1 and y > 1 and y < 6.6:
+        #     return True
+        # # boxes high
+        # if x > -2.5 and x < 1.4 and y > 7.1 and y < 10.2:
+        #     return True
+        # # boxes top right
+        # if x > 2.1 and x < 6.6 and y > 2.6 and y < 10.2:
+        #     return True
+        # # scaffold
+        # if x > 2.2 and x < 6.6 and y > -10 and y < 1.4:
+        #     return True
+        # return False
 
     def update_pose(self, x, y, yaw_degrees):
         next_state = ModelState()
@@ -180,7 +184,7 @@ def main():
         full_pkl = (psm.t_poses, psm.t_ranges, psm.t_images)
         with open('/home/simon/catkin_ws/src/turtlebot3_gazebo/scripts/data/data.pkl', 'wb') as f:
             pickle.dump(full_pkl, f)
-        print('Pickle dumped')
+        print('Final pickle dumped')
     
     except KeyboardInterrupt:
         print("Shutting down")
